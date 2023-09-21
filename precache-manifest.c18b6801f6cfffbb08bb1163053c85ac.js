3ed364b27f7b870e3147eff5a7b9226b99afc159
charanya06 <charanya1784@gmail.com>
2023-09-21 19:08:07 +0530

    Updates

---
+++ precache-manifest.c18b6801f6cfffbb08bb1163053c85ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7c0ec4a4adb6b110466fb5d4262b5e",
+    "url": "/Delici_rest_frontend/index.html"
+  },
+  {
+    "revision": "314471cda7a44cf5a9c3",
+    "url": "/Delici_rest_frontend/static/css/2.5711c12b.chunk.css"
+  },
+  {
+    "revision": "e1a355299df7028c1a41",
+    "url": "/Delici_rest_frontend/static/css/main.fd371dff.chunk.css"
+  },
+  {
+    "revision": "314471cda7a44cf5a9c3",
+    "url": "/Delici_rest_frontend/static/js/2.4bd7125f.chunk.js"
+  },
+  {
+    "revision": "2d68ee0d980bfcc9117aaa1f4b99eb3b",
+    "url": "/Delici_rest_frontend/static/js/2.4bd7125f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1a355299df7028c1a41",
+    "url": "/Delici_rest_frontend/static/js/main.772216a6.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Delici_rest_frontend/static/js/main.772216a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3de4b3e454086c5190d",
+    "url": "/Delici_rest_frontend/static/js/runtime-main.c3860d84.js"
+  }
+]);
